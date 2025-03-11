8e4a97d950fa0fe2b2c5fc9a1b4db854f2fd73ba
kauamatoso862 <66929853+kauamatoso862@users.noreply.github.com>
2025-03-10 21:13:44 -0300

    ex-02.py

--- ex-02.py
+++ ex-02.py
@@ -1,3 +1,4 @@
+# Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
 print("Kauã Matoso") 
 print("Rio de janeiro") 
 print("Duque de Caxias")

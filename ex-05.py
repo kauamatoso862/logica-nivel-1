3520b4513210e81eafafab1ebf15bc22a843cac4
kauamatoso862 <66929853+kauamatoso862@users.noreply.github.com>
2025-03-10 21:10:19 -0300

    ex-05.py

--- ex-05.py
+++ ex-05.py
@@ -1,3 +1,4 @@
+# Frase na tela - Implemente um programa que imprima na tela a frase "O primeiro programa a gente nunca esquece!".
 print("Nota fiscal")
 print("                          ")
 print("Frutas               Valor")

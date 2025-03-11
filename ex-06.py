1c94849825687ff3a4697d206aa07646ac44cfa6
kauamatoso862 <66929853+kauamatoso862@users.noreply.github.com>
2025-03-10 21:18:04 -0300

    ex-06.py

---
+++ ex-06.py
@@ -0,0 +1,14 @@
+#Menu - Elabore um programa que mostre o seguinte menu na tela:
+# Cadastro de Clientes
+# 0 - Fim
+# 1 - Inclui
+# 2 - Altera
+# 3 - Exclui
+# 4 - Consulta
+
+# Opção: 
+print ("0 - Fim")
+print ("1 - Inclui")
+print ("2 - Altera")
+print ("3 - Exclui")
+print ("4 - Consulta")

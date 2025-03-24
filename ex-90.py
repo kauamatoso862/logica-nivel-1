12e298b97e138a21e38fac6b736addb4b83554f9
kauamatoso862 <66929853+kauamatoso862@users.noreply.github.com>
2025-03-24 19:13:01 -0300

    ex-90.py

---
+++ ex-90.py
@@ -0,0 +1,6 @@
+#escreva um software que some dois numeros.
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+soma = numero1 + numero2
+
+print(f"A soma de {numero1} e {numero2} é {soma}.")

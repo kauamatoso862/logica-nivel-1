bf5a7eb4cbb7127e9947bf1aabb95647867c0ecf
kauamatoso862 <66929853+kauamatoso862@users.noreply.github.com>
2025-03-24 19:43:40 -0300

    ex-12.py

---
+++ ex-12.py
@@ -0,0 +1,7 @@
+#Escreva um algoritmo para ler um valor numérico (do teclado) e escrever (na tela) o seu antecessor
+base = float(input("Qual a base do retangulo?:"))
+altura = float(input("Qual a altura do retangulo?:"))
+
+area = (base * altura)
+
+print(f"A área do retangulo é: {area:.2f}")

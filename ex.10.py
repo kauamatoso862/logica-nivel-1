9127d440fce48cbb6e42dcde68d133a101cfadac
kauamatoso862 <66929853+kauamatoso862@users.noreply.github.com>
2025-03-24 19:22:27 -0300

    ex.10.py

---
+++ ex.10.py
@@ -0,0 +1,10 @@
+# problema: escreva um software informe a média aritimética de um aluno que cursou 4 bimestres
+
+nota1 = float(input("Digite a nota do 1º bimestre: "))
+nota2 = float(input("Digite a nota do 2º bimestre: "))
+nota3 = float(input("Digite a nota do 3º bimestre: "))
+nota4 = float(input("Digite a nota do 4º bimestre: "))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print(f"A média é {media:.2f}")

f4291e13a31cf1cd804a77ef1853a0a283cdac43
kauamatoso862 <66929853+kauamatoso862@users.noreply.github.com>
2025-03-24 20:54:01 -0300

    ex-14.py

---
+++ ex-14.py
@@ -0,0 +1,6 @@
+#  Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 
+idade = int(input("Qual a sua idade?"))
+if (idade>10):
+    print("Maior que 10")
+else:
+    print("Menor que 10")
